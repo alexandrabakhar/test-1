d26cbd9fdd4e41f46c6fee2b6d5622c2fc5d8cba
alexandrabakhar <112278780+alexandrabakhar@users.noreply.github.com>
2024-04-10 14:09:43 +0300

    feat: add settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,55 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+    'plugin:unicorn/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: ['@typescript-eslint', 'react-refresh', 'prettier'],
   rules: {
+    'prettier/prettier': 'warn',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
+    ],
+    'no-console': 'warn',
+    'no-duplicate-imports': 'error',
+    'unicorn/filename-case': [
+      'error',
+      {
+        cases: {
+          camelCase: true,
+          pascalCase: true,
+          kebabCase: true,
+        },
+      },
+    ],
+    'unicorn/import-style': [
+      'error',
+      {
+        styles: {
+          path: { named: true },
+          fs: { default: true },
+        },
+        extendDefaultStyles: false,
+      },
+    ],
+    'unicorn/prevent-abbreviations': [
+      'error',
+      {
+        replacements: {
+          env: {
+            environment: false,
+          },
+        },
+      },
     ],
   },
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import { resolve } from 'node:path'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import { fileURLToPath } from 'node:url'
 
-// https://vitejs.dev/config/
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
 })
